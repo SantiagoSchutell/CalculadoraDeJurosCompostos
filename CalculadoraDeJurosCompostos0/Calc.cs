a8164359baa8c851d72a9991bcdf6879bcf41297
SantiagoSchutell <contatosantiagoschutell@gmail.com>
2024-01-28 20:31:30 -0300

    Adicionado Leitura dos dados

---
+++ CalculadoraDeJurosCompostos0/Calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeJurosCompostos0
+{
+    internal class Calc
+    {
+
+        public double Valorinicial { get; set; }
+        public double ValorMensal { get; set; }
+        public int Ano { get; set; }
+        public double Juros { get; set; }
+
+    }
+}
--- CalculadoraDeJurosCompostos0/Program.cs
+++ CalculadoraDeJurosCompostos0/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace CalculadoraDeJurosCompostos0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Calc calc = new Calc();
+
+            Console.Write("Investimento inicial: R$ ");
+            double ValorInicial = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor Mensal: ");
+            double ValorMensal = double.Parse(Console.ReadLine());
+
+
+            Console.Write("Tempo em Meses: ");
+            int Ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Taxa de Juros a.a: ");
+            double Juros = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"No final de {calc.Ano} anos você terá R$ ");
+
         }
     }
 }
